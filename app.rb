707a1f0e3eeb92c88e252d2920a0103a309791c9
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-12 15:30:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name' do
+    @name = params[:name]
+    erb :reversename 
+  end 
+  
+  get '/square/:number' do
+    @num = params[:number].to_i 
+    erb :square 
+  end 
+  
+  get '/say/:number/:phrase' do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :say 
+  end 
+  
+  
+  
 end
