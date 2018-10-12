728e99ad075758464faebbf6904745124cbabc42
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-12 15:30:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,6 +18,12 @@ class App < Sinatra::Base
     erb :say 
   end 
   
-  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @w1 = params[:word1]
+    @w2 = params[:word2]
+    @w3 = params[:word3]
+    @w4 = params[:word4]
+    @w5 = params[:word5]
+    erb :saywords
   
 end
